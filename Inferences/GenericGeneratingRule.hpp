9abc441d50dc0acd73bc8e0b20b0e499673fa7a5
Jakob Rath <git@jakobrath.eu>
2019-02-19 14:21:39 +0100

    Add stub for generic rule

---
+++ Inferences/GenericGeneratingRule.hpp
@@ -0,0 +1,24 @@
+#ifndef GENERICGENERATINGRULE_HPP
+#define GENERICGENERATINGRULE_HPP
+
+#include "InferenceEngine.hpp"
+
+namespace Inferences {
+
+using namespace Kernel;
+using namespace Indexing;
+using namespace Saturation;
+
+
+class GenericGeneratingRule
+    : public GeneratingInferenceEngine
+{
+};
+
+
+
+
+
+}
+
+#endif /* !GENERICGENERATINGRULE_HPP */
