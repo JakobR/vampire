e817faa4a4d982091f4fbd083d21de372a8d8925
Jakob Rath <git@jakobrath.eu>
2019-04-15 12:12:59 +0200

    Make getMappingIterator work with C++11

--- Lib/Metaiterators.hpp
+++ Lib/Metaiterators.hpp
@@ -632,37 +632,16 @@ private:
  *
  * @see MappingIterator
  */
-// template<typename Inner, typename Functor>
-// MappingIterator<Inner,Functor,RETURN_TYPE(Functor)> getMappingIterator(Inner it, Functor f)
-// {
-//   return MappingIterator<Inner,Functor,RETURN_TYPE(Functor)>(it, f);
-// }
-
-/** A version of getMappingIterator that works with C++11 lambdas */
 template< typename Inner
         , typename Functor
         , typename InnerElementType = decltype(std::declval<Inner>().next())
-        , typename ResultType = std::invoke_result_t<Functor, InnerElementType>   // nicer version, but requires C++17
-        // , typename ResultType = decltype(std::declval<Functor>()(std::declval<InnerElementType>()))   // C++11 version of previous line
-        // , typename ResultType = typename std::result_of<Functor(InnerElementType)>::type   // should work in C++11, but deprecated in C++17
+        , typename ResultType = decltype(std::declval<Functor>()(std::declval<InnerElementType>()))
+        // , typename ResultType = std::invoke_result_t<Functor, InnerElementType>   // nicer version of previous line, but requires C++17
         >
 MappingIterator<Inner,Functor,ResultType> getMappingIterator(Inner it, Functor f)
 {
   return MappingIterator<Inner,Functor,ResultType>(it, f);
 }
-
-/**
- * Return iterator that returns elements of @b it transformed by
- * the lambda @b f
- *
- * @see MappingIterator
- */
-// BROKEN???
-//template<typename Inner, typename Functor,typename ResultType>
-//MappingIterator<Inner,Functor,ResultType> getMappingIterator(Inner it, std::function<ResultType(Inner)> f)
-//{
-//  return MappingIterator<Inner,Functor,ResultType>(it, f);
-//}
 
 /**
  * Return iterator that returns elements of @b it transformed by
