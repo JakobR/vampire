6aa7c85e0e58e0b32e42b214051eb86565426ac9
Jakob Rath <git@jakobrath.eu>
2019-08-02 14:43:06 +0200

    Always enable runtime statistics (also in release mode)

--- Debug/RuntimeStatistics.hpp
+++ Debug/RuntimeStatistics.hpp
@@ -24,6 +24,8 @@
 #ifndef __RuntimeStatistics__
 #define __RuntimeStatistics__
 
+#define RUNTIME_STATS 1
+
 /**
 If RUNTIME_STATS is defined as 0, runtime statistics are not being
 collected nor output.
